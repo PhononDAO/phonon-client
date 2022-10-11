31a2b82bfc8af0e1333b4f0ae2d60e58271ec7b5
Anthony Montalbano <anthony.montalbano@gridplus.io>
2022-10-11 17:21:33 -0400

    stage foundation

--- gui/frontend/src/App.tsx
+++ gui/frontend/src/App.tsx
@@ -1,16 +1,14 @@
 import React, { Suspense } from 'react';
-import { useTranslation } from 'react-i18next';
+import { Header } from './components/Header';
 import { NotificationTray } from './components/NotificationTray';
 import { Stage } from './components/Stage';
 
 const App = () => {
-  const { t } = useTranslation();
-
   return (
     <Suspense fallback="loading">
-      <div className="w-100 relative">
-        <NotificationTray />
-        <header className="text-4xl">{t('PHONON MANAGER GOES HERE')}</header>
+      <NotificationTray />
+      <div className="w-100 flex flex-col h-screen relative">
+        <Header />
         <Stage />
       </div>
     </Suspense>

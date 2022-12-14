953bbd7bbecc7a348c1fa0564bdcd993e3ab2418
Anthony Montalbano <anthony.montalbano@gridplus.io>
2022-12-14 14:09:43 -0500

    ability to clear activity history

--- gui/frontend/src/components/GlobalSettingsActivityHistory.tsx
+++ gui/frontend/src/components/GlobalSettingsActivityHistory.tsx
@@ -1,12 +1,15 @@
-import { Stack } from '@chakra-ui/react';
+import { Button, Stack } from '@chakra-ui/react';
 import { useTranslation } from 'react-i18next';
 import localStorage from '../utils/localStorage';
 import { DateTime } from 'luxon';
 import { IonIcon } from '@ionic/react';
 import { checkmarkCircle, closeCircle } from 'ionicons/icons';
+import { useState } from 'react';
 
 export const GlobalSettingsActivityHistory: React.FC = () => {
   const { t } = useTranslation();
+  const [history, setHistory] = useState(localStorage.getActivityHistory());
+
   const theme = {
     success: {
       styles: 'text-green-600 bg-green-200',
@@ -18,37 +21,42 @@ export const GlobalSettingsActivityHistory: React.FC = () => {
     },
   };
 
+  const clearHistory = () => {
+    localStorage.setActivityHistory([]);
+    setHistory([]);
+  };
+
   return (
     <>
-      {localStorage.getActivityHistory().length > 0 && (
-        <div className="mb-2">
+      {history.length > 0 && (
+        <div className="mb-2 flex justify-between items-center">
           {t(
             'The following is your activity history, with the most recent first:'
           )}
+          <Button size="sm" onClick={clearHistory}>
+            {t('Clear activity history')}
+          </Button>
         </div>
       )}
       <Stack className="h-144 overflow-scroll" spacing={3}>
-        {localStorage
-          .getActivityHistory()
-          .reverse()
-          .map((historyItem, key) => (
-            <div
-              key={key}
-              className={
-                'flex  items-center gap-x-2 px-4 py-2 rounded ' +
-                String(theme[historyItem.type].styles)
-              }
-            >
-              <IonIcon size="large" icon={theme[historyItem.type].icon} />
-              <div className="w-full flex justify-between items-center gap-x-4">
-                {historyItem.message}
-                <span className="text-gray-400 text-sm whitespace-nowrap">
-                  {DateTime.fromISO(historyItem.datetime).toRFC2822()}
-                </span>
-              </div>
+        {history.reverse().map((historyItem, key) => (
+          <div
+            key={key}
+            className={
+              'flex  items-center gap-x-2 px-4 py-2 rounded ' +
+              String(theme[historyItem.type].styles)
+            }
+          >
+            <IonIcon size="large" icon={theme[historyItem.type].icon} />
+            <div className="w-full flex justify-between items-center gap-x-4">
+              {historyItem.message}
+              <span className="text-gray-400 text-sm whitespace-nowrap">
+                {DateTime.fromISO(historyItem.datetime).toRFC2822()}
+              </span>
             </div>
-          ))}
-        {localStorage.getActivityHistory().length === 0 && (
+          </div>
+        ))}
+        {history.length === 0 && (
           <span className="text-xl text-gray-500 italic">
             {t('No activity history yet.')}
           </span>
